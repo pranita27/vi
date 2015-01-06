188f15896125a4b4c219c4e8f28951171bccb80f
Pranita Akkatangerhal <pranita.akkatangerhal@tas-analytic.com>
2015-01-06 14:11:03 +0530

    Inserting data to  visits

--- app/controllers/VisitorsController.php
+++ app/controllers/VisitorsController.php
@@ -38,18 +38,7 @@ class VisitorsController extends \BaseController {
 	 */
 	public function store()
 	{
-		if (Input::has('img')) {
-			list($type, $data) = explode(';', Input::get('img'));
-			list(, $data)      = explode(',', $data);
-			$data = str_replace(' ', '+', $data);
-			$data = base64_decode($data);
-			file_put_contents(Config::get('app.visitor_img_path') . '/' . uniqid() . '.jpeg', $data);
-		}
-		;exit;
-
-		//
 		$v = Validator::make(Input::all(), self::getValidationRules());
-		print_r(Input::file('picture'));
 		if ($v->fails()) {
 			print_r($v->failed());
 			print_r($v->messages());
@@ -58,27 +47,44 @@ class VisitorsController extends \BaseController {
 		}
 		else {
 			$visitor = new Visitor(Input::all());
-			$visitor->picture_file_path = 'visitor.png';
-			$dest_dir = Config::get('app.visitor_img_path') . date('/Y/m/d/');
-			if (File::isDirectory($dest_dir)) {
-				if (!File::isWritable($dest_dir)) {
-					//Try to change perms?
+			$visitor->picture_file_path = '';
+			if (Input::has('img')) {
+				list($type, $data) = explode(';', Input::get('img'));
+				list(, $data)      = explode(',', $data);
+				$data = str_replace(' ', '+', $data);
+				$data = base64_decode($data);
+				if ($data) {
+					$dest_dir = Config::get('app.visitor_img_path') . date('/Y/m/d/');
+					if (File::isDirectory($dest_dir)) {
+						if (!File::isWritable($dest_dir)) {
+							//Try to change perms?
+						}
+					}
+					else {
+						File::makeDirectory($dest_dir, 0775, true);
+					}
+					$rand = '';
+					do {
+						$file_name = Str::slug(Input::get('name')) . $rand . '.jpeg';
+						$rand = Str::random(4);
+					} while (File::exists($dest_dir . $file_name));
+					if (file_put_contents($dest_dir . $file_name, $data)) {
+						$visitor->picture_file_path = $file_name;
+					}
 				}
 			}
-			else {
-				File::makeDirectory($dest_dir, 0775, true);
+			//
+			if (!$visitor->save()) {
+				App::abort(500, 'Oops!! Something went wrong.');	
 			}
-			$rand = '';
-			do {
-				$file_name = Str::slug(Input::get('name')) . $rand . '.' . File::extension(Input::file('picture')->getClientOriginalName());
-				$rand = Str::random(4);
-			} while (File::exists($dest_dir . '/' . $file_name));
-			Input::file('picture')->move($dest_dir, $file_name);
-			if ($visitor->save()) {
-				echo 'saved with ' . $visitor->id;
-			}
-		}
 
+			$visit = new Visit(Input::all());
+			$visit->visitor_id = $visitor->id;
+			if (!$visit->save()) {
+				App::abort(500, 'Oops!! Something went wrong.');
+			}
+			
+		}
 
 	}
 
@@ -137,7 +143,7 @@ class VisitorsController extends \BaseController {
 			'phone' => ['required', 'regex:/^((\+91)|0?)[7-9][0-9]{9}$/', 'unique:visitors'],
 			'email' => 'required|between:5,150|email|unique:visitors',
 			'from' => 'required|between:3,100',
-			'picture' => 'required|mimes:jpeg,png|between:4,400',
+			// 'picture' => 'required|mimes:jpeg,png|between:4,400',
 		];
 		return empty($selected) ? $rules : array_only($rules, $selected);
 	}
